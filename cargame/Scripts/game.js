f13741cbb2c14882c90926b44ee17e3ce3c51e72
ebin-antoo <ebin@ebin-antoo.com>
2014-11-10 16:44:00 -0500

    bassics of the game

--- cargame/Scripts/game.js
+++ cargame/Scripts/game.js
@@ -4,6 +4,13 @@ var queue;
 //game objects
 var car;
 var coin;
+var road;
+
+//barrier array
+var barrier = [];
+
+//Game constants
+var BARRIER_NUM = 3;
 
 function preload() {
     queue = new createjs.LoadQueue();
@@ -12,6 +19,8 @@ function preload() {
     queue.loadManifest([
         { id: "car", src: "assets/images/car.png" },
         { id: "coin", src: "assets/images/coin.png" },
+        { id: "barrier", src: "assets/images/road-barrier.png" },
+        { id: "road", src: "assets/images/road.jpg" },
         { id: "yay", src: "assets/sounds/yay.ogg" }
     ]);
 }
@@ -20,14 +29,18 @@ function init() {
     stage = new createjs.Stage(document.getElementById("canvas"));
     stage.enableMouseOver(20);
     createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener("tick", gameloop);
+    createjs.Ticker.addEventListener("tick", raceloop);
     gameStart();
 }
 
-function gameloop(event) {
+function raceloop(event) {
+    road.update();
     coin.update();
     car.update();
 
+    for (var count = 0; count < BARRIER_NUM; count++) {
+        barrier[count].update();
+    }
     stage.update();
 }
 
@@ -76,19 +89,66 @@ var Coin = (function () {
     return Coin;
 })();
 
+//Barriers class
+var Barrier = (function () {
+    function Barrier() {
+        this.Image = new createjs.Bitmap(queue.getResult("barrier"));
+        this.width = this.Image.getBounds().width;
+        this.height = this.Image.getBounds().height;
+        this.Image.regX = this.width * 0.5;
+        this.Image.regY = this.width * 0.5;
+
+        stage.addChild(this.Image);
+        this.reset();
+    }
+    Barrier.prototype.reset = function () {
+        this.Image.y = -this.height;
+        this.Image.x = Math.floor(Math.random() * stage.canvas.width);
+        this.dy = Math.floor(Math.random() * 5 + 5);
+        this.dx = Math.floor(Math.random() * 4 - 2);
+    };
+
+    Barrier.prototype.update = function () {
+        this.Image.y += this.dy;
+        this.Image.x += this.dx;
+        if (this.Image.y >= (this.height + stage.canvas.height)) {
+            this.reset();
+        }
+    };
+    return Barrier;
+})();
+
+//Road class
+var Road = (function () {
+    function Road() {
+        this.Image = new createjs.Bitmap(queue.getResult("road"));
+        this.width = this.Image.getBounds().width;
+        this.height = this.Image.getBounds().height;
+        this.dy = 5;
+
+        stage.addChild(this.Image);
+        this.reset();
+    }
+    Road.prototype.reset = function () {
+        this.Image.y = -960;
+    };
+
+    Road.prototype.update = function () {
+        this.Image.y += this.dy;
+        if (this.Image.y >= 0) {
+            this.reset();
+        }
+    };
+    return Road;
+})();
+
 function gameStart() {
+    road = new Road();
     coin = new Coin();
     car = new Car();
-    /* // Add code here
-    
-    // Some example code here - to be replaced
-    var placeholder = new createjs.Bitmap(queue.getResult('loading'));
-    placeholder.regX = placeholder.image.width / 2;
-    placeholder.regY = placeholder.image.height / 2;
-    placeholder.x = stage.canvas.width / 2;
-    placeholder.y = stage.canvas.height / 2;
-    stage.addChild(placeholder);
-    createjs.Sound.play("yay");
-    */
+
+    for (var count = 0; count < BARRIER_NUM; count++) {
+        barrier[count] = new Barrier();
+    }
 }
 //# sourceMappingURL=game.js.map
--- cargame/Scripts/game.ts
+++ cargame/Scripts/game.ts
@@ -4,6 +4,13 @@ var queue;
 //game objects
 var car: Car;
 var coin: Coin;
+var road: Road;
+
+//barrier array
+var barrier = [];
+
+//Game constants
+var BARRIER_NUM: number = 3;
 
 function preload(): void {
     queue = new createjs.LoadQueue();
@@ -12,6 +19,8 @@ function preload(): void {
     queue.loadManifest([
         { id: "car", src: "assets/images/car.png" },
         { id: "coin", src: "assets/images/coin.png" },
+        { id: "barrier", src: "assets/images/road-barrier.png" },
+        { id: "road", src: "assets/images/road.jpg" },
         { id: "yay", src: "assets/sounds/yay.ogg" }
     ]);
 }
@@ -20,14 +29,18 @@ function init(): void {
     stage = new createjs.Stage(document.getElementById("canvas"));
     stage.enableMouseOver(20);
     createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener("tick", gameloop);
+    createjs.Ticker.addEventListener("tick", raceloop);
     gameStart();
 }
 
-function gameloop(event): void {
+function raceloop(event): void {
+    road.update();
     coin.update();
     car.update();
 
+    for (var count = 0; count < BARRIER_NUM; count++) {
+        barrier[count].update();
+    }
     stage.update();
 }
 
@@ -84,20 +97,75 @@ class Coin {
     }
 } //EO coin class
 
+//Barriers class
+class Barrier {
+    Image: createjs.Bitmap;
+    width: number;
+    height: number;
+    dy: number;
+    dx: number;
+    constructor() {
+        this.Image = new createjs.Bitmap(queue.getResult("barrier"));
+        this.width = this.Image.getBounds().width;
+        this.height = this.Image.getBounds().height;
+        this.Image.regX = this.width * 0.5;
+        this.Image.regY = this.width * 0.5;
+
+        stage.addChild(this.Image);
+        this.reset();
+    }
+
+    reset() {
+        this.Image.y = -this.height;
+        this.Image.x = Math.floor(Math.random() * stage.canvas.width);
+        this.dy = Math.floor(Math.random() * 5 + 5);
+        this.dx = Math.floor(Math.random() * 4 - 2);
+    }
+
+    update() {
+        this.Image.y += this.dy;
+        this.Image.x += this.dx;
+        if (this.Image.y >= (this.height + stage.canvas.height)) {
+            this.reset();
+        }
+    }
+} //EO barrier class
+
+//Road class
+class Road {
+    Image: createjs.Bitmap;
+    width: number;
+    height: number;
+    dy: number;
+    constructor() {
+        this.Image = new createjs.Bitmap(queue.getResult("road"));
+        this.width = this.Image.getBounds().width;
+        this.height = this.Image.getBounds().height;
+        this.dy = 5;
+
+        stage.addChild(this.Image);
+        this.reset();
+    }
+
+    reset() {
+        this.Image.y = -960;
+    }
+
+    update() {
+        this.Image.y += this.dy;
+        if (this.Image.y >= 0) {
+            this.reset();
+        }
+    }
+} //EO coin class
+
 function gameStart(): void {
 
+    road = new Road();
     coin = new Coin();
     car = new Car();
 
-   /* // Add code here
-
-    // Some example code here - to be replaced
-    var placeholder = new createjs.Bitmap(queue.getResult('loading'));
-    placeholder.regX = placeholder.image.width / 2;
-    placeholder.regY = placeholder.image.height / 2;
-    placeholder.x = stage.canvas.width / 2;
-    placeholder.y = stage.canvas.height / 2;
-    stage.addChild(placeholder);
-    createjs.Sound.play("yay");
-*/
+    for (var count = 0; count < BARRIER_NUM; count++) {
+        barrier[count] = new Barrier();
+    }
 }
